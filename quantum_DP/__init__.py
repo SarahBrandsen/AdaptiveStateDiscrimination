af2317bb13f8ac5f6a3c2c26c8d17f85237f6224
Mengke Lian <mengkelian@Mengkes-MacBook-Pro.local>
2019-02-04 16:35:31 -0500

    fix typo

--- quantum_DP/__init__.py
+++ quantum_DP/__init__.py
@@ -1,4 +1,4 @@
 from quantum_DP.utils import *
-from quantum_DP.algorithm import DP_Node, quantum_DP
+from quantum_DP.algorithm import DP_Node, Quantum_DP
 from quantum_DP.action_space import *
 from quantum_DP.experiment_manager import ExperimentManager
